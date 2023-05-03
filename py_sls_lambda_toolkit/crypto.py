c321a58548ece6db994f4f76cbb1e07ae955f038
Julio Flores <juliocesarflores12@gmail.com>
2023-05-03 12:03:57 -0400

    feat: update

--- py_sls_lambda_toolkit/crypto.py
+++ py_sls_lambda_toolkit/crypto.py
@@ -1,47 +1,16 @@
-import bcrypt
-import re
+import hashlib
+
+def hash_password(password, salt):
+  """Hashes a password using SHA-256"""
+  password = password.encode('utf-8')
+  salt = salt.encode('utf-8')
+  hashed_password = hashlib.sha256(password + salt).hexdigest()
+  return hashed_password
 
 
-def validate_password(password):
-    """Validates a password against a pattern"""
-    if not isinstance(password, str):
-        raise TypeError("Password must be a string.")
-    
-    # Define a pattern for a valid password
-    # At least 8 characters
-    # At least one uppercase letter
-    # At least one lowercase letter
-    # At least one digit
-    # At least one special character
-    pattern = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\\-=[\]{};':\"\\|,.<>/?]).{8,}$"
-    
-    if not re.match(pattern, password):
-        raise ValueError("Invalid password.")
-        
-
-def encrypt_password(password):
-    """Encrypts a password using bcrypt"""
-    if not isinstance(password, str):
-        raise TypeError("Password must be a string.")
-    
-    password_bytes = password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-    return hashed_password
-
-
-def decrypt_password(password, hashed_password):
-    """Decrypts a password using bcrypt"""
-    if not isinstance(password, str):
-        raise TypeError("Password must be a string.")
-    if not isinstance(hashed_password, bytes):
-        raise TypeError("Hashed password must be bytes.")
-    
-    password_bytes = password.encode('utf-8')
-    return bcrypt.checkpw(password_bytes, hashed_password)
-
-
-def validate_decrypted_password(password, hashed_password):
-    """Validates a password against a hashed password"""
-    if not decrypt_password(password, hashed_password):
-        raise ValueError("Invalid password.")
+def verify_password(password, hashed_password, salt):
+  """Verifies a password by comparing the hashed password to the provided password"""
+  password = password.encode('utf-8')
+  salt = salt.encode('utf-8')
+  hashed_password_to_check = hashlib.sha256(password + salt).hexdigest()
+  return hashed_password == hashed_password_to_check
