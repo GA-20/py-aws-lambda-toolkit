9eb88cd8a7e2592a87d47165228e60ec4824056f
Julio Flores <juliocesarflores12@gmail.com>
2023-07-01 18:32:47 -0400

    test: process event

--- __tests__/test_http_event.py
+++ __tests__/test_http_event.py
@@ -1,59 +1,42 @@
-from py_sls_lambda_toolkit.logger import logging
-from typing import Dict
-from py_sls_lambda_toolkit.parsers import CamelToSnakeConverter
+import unittest
+from py_sls_lambda_toolkit.http_event import process_event
 
-
-def process_event(event: Dict) -> Dict:
-    """
-    Converts the keys in the `query_string_parameters`, `path_parameters`, and `body` dictionaries from camel case to snake case.
-
-    Args:
-        event (Dict): A dictionary representing the input event.
-
-    Returns:
-        Dict: A dictionary with the same keys as the input dictionary, but with the keys in the `query_string_parameters`, `path_parameters`, and `body` dictionaries converted from camel case to snake case.
-
-    Raises:
-        Exception: If an exception occurs during the conversion process.
-    """
-    try:
-        body = event.get('body')
-        headers = event.get('headers')
-        http_method = event.get('httpMethod')
-        is_base64_encoded = event.get('isBase64Encoded')
-        multi_value_headers = event.get('multiValueHeaders')
-        path = event.get('path')
-        path_parameters = event.get('pathParameters')
-        query_string_parameters = event.get('queryStringParameters')
-        request_context = event.get('requestContext')
-        resource = event.get('resource')
-        stage_variables = event.get('stageVariables')
-
-        logging.info({
-            'requestContext': request_context,
-            'httpMethod': http_method,
-            'path': path,
-            'headers': headers
-        })
-
-        query_string_parameters = CamelToSnakeConverter.convert(
-            query_string_parameters)
-        path_parameters = CamelToSnakeConverter.convert(path_parameters)
-        body = CamelToSnakeConverter.convert(body)
-
-        return {
-            'body': body,
-            'headers': headers,
-            'httpMethod': http_method,
-            'isBase64Encoded': is_base64_encoded,
-            'multiValueHeaders': multi_value_headers,
-            'path': path,
-            'pathParameters': path_parameters,
-            'queryStringParameters': query_string_parameters,
-            'requestContext': request_context,
-            'resource': resource,
-            'stageVariables': stage_variables
+class TestProcessEvent(unittest.TestCase):
+    def test_process_event(self):
+        event = {
+            'body': '{"name": "John", "age": 30}',
+            'headers': {'Content-Type': 'application/json'},
+            'httpMethod': 'POST',
+            'isBase64Encoded': False,
+            'multiValueHeaders': {'Accept': ['*/*']},
+            'path': '/users',
+            'pathParameters': {'id': '123'},
+            'queryStringParameters': {'sort': 'desc'},
+            'requestContext': {'accountId': '123456789012', 'resourceId': '123456', 'stage': 'prod'},
+            'resource': '/users/{id}',
+            'stageVariables': {'env': 'prod'}
         }
-    except Exception as e:
-        logging.error(e)
-        raise e
+
+        expected_output = {
+            'body': {'name': 'John', 'age': 30},
+            'headers': {'Content-Type': 'application/json'},
+            'httpMethod': 'POST',
+            'isBase64Encoded': False,
+            'multiValueHeaders': {'Accept': ['*/*']},
+            'path': '/users',
+            'pathParameters': {'id': '123'},
+            'queryStringParameters': {'sort': 'desc'},
+            'requestContext': {'accountId': '123456789012', 'resourceId': '123456', 'stage': 'prod'},
+            'resource': '/users/{id}',
+            'stageVariables': {'env': 'prod'}
+        }
+
+        try:
+            output = process_event(event)
+        except Exception as e:
+            self.fail(e)
+
+        self.assertEqual(output, expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
