ceb5ff4a5a19e54e8b7b6acd2eab638e5aeb2c66
Julio Flores <juliocesarflores12@gmail.com>
2023-07-01 21:46:19 -0400

    test: logger

---
+++ __tests__/test_logger.py
@@ -0,0 +1,11 @@
+import unittest
+from py_sls_lambda_toolkit.logger import logger
+
+class TestLogger(unittest.TestCase):
+
+    def test_logger(self,):
+        logger.info('test logger')
+        logger.error('test logger')
+        logger.debug('test logger')
+        logger.warning('test logger')
+        self.assertEqual(True, True)
--- py_sls_lambda_toolkit/logger.py
+++ py_sls_lambda_toolkit/logger.py
@@ -2,3 +2,5 @@ import logging
 
 log_format = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(format=log_format)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
