de7a304ca64ca7d94ce88e6b041bbaad2980d5d9
Julio Flores <juliocesarflores12@gmail.com>
2023-07-04 08:50:36 -0400

    feat: snake to camel case

---
+++ __tests__/test_parsers.py
@@ -0,0 +1,34 @@
+import unittest
+from py_sls_lambda_toolkit.parsers import (
+    convert_camel_to_snake,
+    convert_snake_to_camel,
+)
+
+
+class TestParsers(unittest.TestCase):
+
+    def test_convert_camel_to_snake(self):
+        data = {
+            "testKey": "testValue",
+            "testKey2": "testValue2",
+            "testKey3": "testValue3",
+        }
+        expected = {
+            "test_key": "testValue",
+            "test_key2": "testValue2",
+            "test_key3": "testValue3",
+        }
+        self.assertEqual(convert_camel_to_snake(data), expected)
+
+    def test_convert_snake_to_camel(self):
+        data = {
+            "test_key": "testValue",
+            "test_key2": "testValue2",
+            "test_key3": "testValue3",
+        }
+        expected = {
+            "testKey": "testValue",
+            "testKey2": "testValue2",
+            "testKey3": "testValue3",
+        }
+        self.assertEqual(convert_snake_to_camel(data), expected)
--- py_sls_lambda_toolkit/parsers.py
+++ py_sls_lambda_toolkit/parsers.py
@@ -3,27 +3,23 @@ from .logger import logging
 import json
 
 
-class SnakeToCamelConverter:
-    @staticmethod
-    def convert(data):
-        try:
-            if data:
-                if isinstance(data, str):
-                    return dict_to_camel(json.loads(data))
-                return dict_to_camel(data)
-        except Exception as e:
-            logging.error(e)
-            raise e
+def convert_snake_to_camel(data):
+    try:
+        if data:
+            if isinstance(data, str):
+                return dict_to_camel(json.loads(data))
+            return dict_to_camel(data)
+    except Exception as e:
+        logging.error(e)
+    raise e
 
 
-class CamelToSnakeConverter:
-    @staticmethod
-    def convert(data):
-        try:
-            if data:
-                if isinstance(data, str):
-                    return dict_to_snake(json.loads(data))
-                return dict_to_snake(data)
-        except Exception as e:
-            logging.error(e)
-            raise e
+def convert_camel_to_snake(data):
+    try:
+        if data:
+            if isinstance(data, str):
+                return dict_to_snake(json.loads(data))
+        return dict_to_snake(data)
+    except Exception as e:
+        logging.error(e)
+        raise e
